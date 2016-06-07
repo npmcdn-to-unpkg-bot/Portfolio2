9ea60774fcaef36df5ff6175474ef369357be26a
Alexander Pinkerton <alexpinkerton88@gmail.com>
2016-06-07 19:30:27 -0400

    Removed some magic numbers

--- main/application/components/parallax.ts
+++ main/application/components/parallax.ts
@@ -17,6 +17,13 @@ export class Parallax {
     this.lastY = 0;
     this.offsetY = -2500;
     this.offsetX = -800;
+    
+    //Parallax Distances
+    this.minDistance = 25;
+    this.distanceLayer1 = 20;
+    this.distanceLayer2 = 10;
+    this.distanceLayer3 = 5;
+    this.distanceLayer4 = 2;
   }
 
   
@@ -26,17 +33,17 @@ export class Parallax {
   moveLayers(event) {
 
     //Move background if mouse has moved minimum distance to save computation
-    if (Math.abs(event.pageX - this.lastX) > 25 || Math.abs(event.pageY - this.lastY) > 25) {
+    if (Math.abs(event.pageX - this.lastX) > this.minDistance || Math.abs(event.pageY - this.lastY) > this.minDistance) {
       
       //Apply Offset
       var x = -(event.pageX + this.offsetY);
       var y = -(event.pageY + this.offsetX);
 
       //Move the background
-      document.getElementsByClassName('layer1')[0].style.backgroundPosition = x / 20 + 'px ' + y / 20 + 'px';
-      document.getElementsByClassName('layer2')[0].style.backgroundPosition = x / 10 + 'px ' + y / 10 + 'px';
-      document.getElementsByClassName('layer3')[0].style.backgroundPosition = x / 5 + 'px ' + y / 5 + 'px';
-      document.getElementsByClassName('layer4')[0].style.backgroundPosition = x / 2 + 'px ' + y / 2 + 'px';
+      document.getElementsByClassName('layer1')[0].style.backgroundPosition = x / this.distanceLayer1 + 'px ' + y / this.distanceLayer1 + 'px';
+      document.getElementsByClassName('layer2')[0].style.backgroundPosition = x / this.distanceLayer2 + 'px ' + y / this.distanceLayer2 + 'px';
+      document.getElementsByClassName('layer3')[0].style.backgroundPosition = x / this.distanceLayer3 + 'px ' + y / this.distanceLayer3 + 'px';
+      document.getElementsByClassName('layer4')[0].style.backgroundPosition = x / this.distanceLayer4 + 'px ' + y / this.distanceLayer4 + 'px';
 
       //Update the last X and Y where a shift occurred.
       this.lastX = event.pageX;
