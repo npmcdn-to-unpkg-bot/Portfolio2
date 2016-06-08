0c3bbe263e992c1be7930b98584a3d69632d266d
Alexander Pinkerton <alexpinkerton88@gmail.com>
2016-06-08 17:58:34 -0400

    working on css for placement of pictures

--- main/application/components/portfolio.ts
+++ main/application/components/portfolio.ts
@@ -18,42 +18,43 @@ class Portfolio {
 
   constructor() {
 
-    //$('#pagepiling').pagepiling();
     $('#pagepiling').pagepiling({
-	    menu: null,
-        direction: 'horizontal',
-        verticalCentered: true,
-        sectionsColor: [],
-        anchors: [],
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#000',
-            'bulletsColor': '#000',
-            'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4']
-        },
-       	normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
+      menu: null,
+      direction: 'horizontal',
+      verticalCentered: true,
+      sectionsColor: [],
+      anchors: [],
+      scrollingSpeed: 700,
+      easing: 'swing',
+      loopBottom: false,
+      loopTop: false,
+      css3: true,
+      navigation: {
+        'textColor': '#000',
+        'bulletsColor': '#000',
+        'position': 'right',
+        'tooltips': ['section1', 'section2', 'section3', 'section4']
+      },
+      normalScrollElements: null,
+      normalScrollElementTouchThreshold: 5,
+      touchSensitivity: 5,
+      keyboardScrolling: true,
+      sectionSelector: '.section',
+      animateAnchor: false,
 
-		//events
-		onLeave: function(index, nextIndex, direction){
-      
-    },
-		afterLoad: function(anchorLink, index){
-      console.log('loaded');
-    },
-		afterRender: function(){},
-	});
+      //events
+      onLeave: function (index, nextIndex, direction) {
+
+      },
+      afterLoad: function (anchorLink, index) {
+        console.log('loaded');
+      },
+      afterRender: function () { },
+    });
 
   }
+
+
 
 }
 
